7c8d71bce5c18bee2e9c6c9564ed77001cce3c7c
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-02-18 13:04:51 +0600

    using fraction number

--- convert.js
+++ convert.js
@@ -5,3 +5,18 @@ function inchToFeet(inch){
 }
 let jackHeight= inchToFeet(80);
 console.log("The height of Jack is "+jackHeight+" feets.");
+
+
+// How to use fractional digits
+
+function inchToFeet2(inch){
+    const feetFraction = inch/12;
+    const feetN= parseInt(feetFraction);
+    const inc = inch%12;
+    const results = feetN + '  feet ' + inc + ' inch.';
+    return results;
+
+}
+
+let jackHeight2= inchToFeet2(80);
+console.log("The height of Jack is "+jackHeight2);
