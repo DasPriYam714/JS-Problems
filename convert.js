93a2913f1796653e986aee59a8eaac200e1a84fe
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-02-18 13:26:10 +0600

    leap year or not

--- convert.js
+++ convert.js
@@ -20,3 +20,24 @@ function inchToFeet2(inch){
 
 let jackHeight2= inchToFeet2(80);
 console.log("The height of Jack is "+jackHeight2);
+
+// check a year leap year or not.
+
+function yearLeapYear(year){
+    if(year % 100 !== 0 && year % 4=== 0){
+        return true;
+    }
+    else if(year % 100 === 0 && year % 400 ===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const isLeap = yearLeapYear(1900);
+const isLeap1 = yearLeapYear(1920);
+const isLeap2 = yearLeapYear(2010);
+const isLeap3 = yearLeapYear(2060);
+
+console.log(isLeap, isLeap1, isLeap2,isLeap3);
